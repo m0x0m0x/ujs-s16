71a20fcb4c4b9c93c4b4637ffcee93d7ebe63caf
m0x0m0x <mx0mx@tuta.io>
2024-05-14 00:01:12 +0530

    adding promises

---
+++ L252/sc3.js
@@ -0,0 +1,15 @@
+// Code for 252
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+/////////////// Actual Code here
+
+//Old method
+// const request = new XMLHttpRequest();
+// request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+const request = fetch("https://restcountries.com/v3.1/name/yemen");
+console.log(request);
