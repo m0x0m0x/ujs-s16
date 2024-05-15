0a883b46d0cecb8afa1b1c4a29ee3db9669df00c
m0x0m0x <mx0mx@tuta.io>
2024-05-15 20:07:56 +0530

    keepoldcode thenmakefunctionsforjson

--- L255/sc3.js
+++ L255/sc3.js
@@ -56,28 +56,78 @@ console.log(request);
 //     });
 // };
 
+// Getting Json Function
+const getJSON = function (url, errorMsg = "😡Fucked ") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+    return response.json();
+  });
+};
+
+// const getCountryData = function (country) {
+//   // Handling fulfiled response
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     //country1
+//     .then((response) => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`No Fuckiung Country Like that ${response.status}`);
+//       return response.json();
+//     })
+//     .then((data) => {
+//       renderCountry(data[0]);
+//       // const neig = data[0].borders[0];
+//       const neig = "rape3";
+
+//       if (!neig) return;
+
+//       //   // Country neighbor - Country 2
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neig}`);
+//     })
+//     .then((response) => {
+//       console.log(response);
+
+//       if (!response.ok)
+//         throw new Error(`No Fuckiung Neighbor Like that ${response.status}`);
+//       return response.json();
+//     })
+//     .then((data) => renderCountry(data[0], "neighbour"))
+//     .catch((error) => {
+//       console.error(error);
+//       renderError(`FuckedUpBastard 🤬🤬 ${error.message}`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+// const getJSON = function (url, errorMsg = "😡Fucked ") {
+//   return fetch(url).then((response) => {
+//     if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+//     return response.json();
+//   });
+// };
+
 const getCountryData = function (country) {
   // Handling fulfiled response
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    //country1
-    .then((response) => {
-      console.log(response);
 
-      if (!response.of)
-        throw new Error(`No Fuckiung Country Like that ${response.status}`);
-
-      return response.json();
-    })
+  getJSON(
+    `https://restcountries.com/v3.1/name/${country}`,
+    "NoFuckingCountry😡😡"
+  )
     .then((data) => {
       renderCountry(data[0]);
-      const neig = data[0].borders[0];
+      // const neig = data[0].borders[0];
+      const neig = "rape3";
 
       if (!neig) return;
 
       //   // Country neighbor - Country 2
-      return fetch(`https://restcountries.com/v3.1/alpha/${neig}`);
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neig}`,
+        "NoNeighBorBastard😡😡"
+      );
     })
-    .then((response) => response.json())
     .then((data) => renderCountry(data[0], "neighbour"))
     .catch((error) => {
       console.error(error);
@@ -92,4 +142,4 @@ btn.addEventListener("click", function () {
   getCountryData("yemen");
 });
 
-getCountryData("adadsdd");
+// getCountryData("adadsdd");
