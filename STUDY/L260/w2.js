c9d0e7aaea379c43fc94ab1754c6b35ad391f4af
m0x0m0x <mx0mx@tuta.io>
2024-05-25 22:05:09 +0530

    make changes

--- STUDY/L260/w2.js
+++ STUDY/L260/w2.js
@@ -4,29 +4,29 @@ Ch 260: Work here
 
 // Import Zone
 import { mainHead } from "./text.js";
-import { console, originalConsoleLog } from "./pcol.js";
+
 ////////////////////
 
 mainHead("Manually Building Promises");
 
 // console.log("Fancy Panty");
 
-console("Sucking and fucking");
+console.log("Sucking and fucking");
 /* 
 Actual work from the chapter
 */
-originalConsoleLog("Booty");
+console.log("Booty");
 setTimeout(() => console.log("0 Sec Timer"), 0);
 
 // This will have immediate resolution
-Promise.resolve("Res1").then((res) => originalConsoleLog(res));
+Promise.resolve("Res1").then((res) => console.log(res));
 
 // Another immediate promise resolution
 Promise.resolve("pussy").then((pussy) => {
   for (let i = 0; i < 1000; i++) {
-    originalConsoleLog(`Number Panty ${i}`);
+    console.log(`Number Panty ${i}`);
   }
-  originalConsoleLog(pussy);
+  console.log(pussy);
 });
 
 console.log("Test End");
