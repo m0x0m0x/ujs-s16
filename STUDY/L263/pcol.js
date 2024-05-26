32054247f5012909f5a7bc973fef40a2cea6c5c3
m0x0m0x <mx0mx@tuta.io>
2024-05-26 09:58:59 +0530

    L263

---
+++ STUDY/L263/pcol.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const consoleOutput = document.getElementById("console-output");
+
+const originalConsoleLog = console.log;
+const customConsoleLog = (...args) => {
+  originalConsoleLog(...args); // Call the original console.log()
+
+  const log = args
+    .map((arg) => JSON.stringify(arg, null, 2))
+    .join(" ")
+    .replace(/\"/g, "&quot;");
+  consoleOutput.innerHTML += `${log}\n`;
+};
+
+export { customConsoleLog as consoleO };
+export { originalConsoleLog };
---
+++ STUDY/L263/sc2.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+///////////////////////////////////////
+
+// Exporting the html fragments into its own function
+
+const renderCountry = function (data, className = "") {
+  const html = `
+        <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.official}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 10000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.ara}</p>
+            <p class="country__row"><span>💰</span>${data.currencies.YER}</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+// Old chool way
+const getCountryAndNei = function (country) {
+  // AJAX call Country 1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    //   console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+
+    console.log(data);
+
+    // Render Country 1
+    renderCountry(data);
+
+    // Get neighbor country (2)
+    const [neig] = data.borders;
+    if (!neig) return;
+
+    // AJAX Call 2
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neig}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      //   console.log(this.responseText);
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, "neighbour");
+    });
+  });
+};
+
+getCountryAndNei("yemen");
+// getCountryData("japan");
+// getCountryData("syria");
